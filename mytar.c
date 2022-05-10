002a011d3b316a84cb0fd5590f7c6fc67e641a5f
Min Hset <hlaing@calpoly.edu>
2022-05-09 17:56:57 -0700

    code for checking options "[ctxvS]f"

--- mytar.c
+++ mytar.c
@@ -3,16 +3,52 @@
 
 int main(int argc, char *argv[]){
 
+    char *options;
+    int num_ops, verbose, idx = 1;
+
     if (argc < 3){
-        fprintf(stderr, "Usage: mytar [ctxvS]f tarfile [ path [ ... ] ]");
+        fprintf(stderr, "Usage: mytar [ctxvS]f tarfile [ path [ ... ] ]\n");
         exit(EXIT_FAILURE);
     }
-    char *options = argv[1];
-    int num_ops = len(options);
+    options = argv[1];
+    num_ops = len(options);
 
     if (num_ops < 2 || num_ops > 4){
-        fprintf(stderr, "Usage: mytar [ctxvS]f tarfile [ path [ ... ] ]");
+        fprintf(stderr, "Usage: mytar [ctxvS]f tarfile [ path [ ... ] ]\n");
         exit(EXIT_FAILURE);
     }
 
+    if (options[0] != 'c' || options[0] != 't' || options[0] != 'x'){
+        fprintf(stderr, "Usage: mytar [ctxvS]f tarfile [ path [ ... ] ]\n");
+        printf("second argument requires a c, t or x as first char\n");
+        exit(EXIT_FAILURE);
+    }
+
+    while (options[idx] != 'f'){
+
+        /* we hit the end of the second argument without encountering an 'f' */
+        if (options[idx] == '\0'){
+            fprintf(stderr, "Usage: mytar [ctxvS]f tarfile [ path [ ... ] ]\n");
+            printf("f required in second argument\n")
+            exit(EXIT_FAILURE);
+        }
+        else if (options[idx] == 'v'){
+            verbose = 1;
+        }
+        else if(options[idx] == 'S'){
+        /* add code to check for standards compliance */
+        }
+
+        else{
+            fprintf(stderr, "Usage: mytar [ctxvS]f tarfile [ path [ ... ] ]\n");
+            printf("Invalid character in second argument\n");
+            exit(EXIT_FAILURE);
+        }
+        idx++;
+    }
+
+
+
+
+    return 0;
 }
