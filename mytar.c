1d8e2cc1e87bab7cfe65dad18fa7ad70511ce659
Min Hset <hlaing@calpoly.edu>
2022-05-09 17:22:00 -0700

    checking tabs and commit

--- mytar.c
+++ mytar.c
@@ -3,10 +3,16 @@
 
 int main(int argc, char *argv[]){
 
-    if (argc < 3 || argc > 4){
+    if (argc < 3){
         fprintf(stderr, "Usage: mytar [ctxvS]f tarfile [ path [ ... ] ]");
         exit(EXIT_FAILURE);
     }
-    char *options = argv[2];
+    char *options = argv[1];
+    int num_ops = len(options);
+
+    if (num_ops < 2 || num_ops > 4){
+        fprintf(stderr, "Usage: mytar [ctxvS]f tarfile [ path [ ... ] ]");
+        exit(EXIT_FAILURE);
+    }
 
 }
