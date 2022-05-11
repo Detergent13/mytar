55df3d7f6960598cd63c3d7c252fc27636be0fff
Detergent13 <dcrush@calpoly.edu>
2022-05-10 20:34:23 -0700

    Add header struct and util.c

---
+++ header.h
@@ -0,0 +1,20 @@
+struct header {
+    char name[100];
+    char mode[8];
+    char uid[8];
+    char gid[8];
+    char size[12];
+    char mtime[12];
+    char chksum[8];
+    char typeflag[1];
+    char linkname[100];
+    char magic[6];
+    char version[2];
+    char uname[32];
+    char gname[32];
+    char devmajor[8];
+    char devminor[8];
+    char prefix[155];
+    /* Here to make it easier to read/write sizeof(header) - full disc block */
+    char padding[12];
+};
